0d5c55c9e94a6b1ad1ebb2ab3e256a54c00d41b5
Vasily Glazkov <vvg.doc@yandex.ru>
2023-11-26 17:01:32 +0200

    added CityList & CityItem components

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,44 @@ import Homepage from "./pages/Homepage/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout/AppLayout.jsx";
 import Login from "./pages/Login/Login.jsx";
+import CityList from "./components/CityList/CityList.jsx";
+import {useEffect, useState} from "react";
+
+const URL = "http://localhost:9000/cities";
 
 export default function App() {
+    const [cities, setCities] = useState([]);
+    const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+        async function fetchCities() {
+            try {
+                setLoading(true);
+                const result = await fetch(URL);
+                const data = await result.json();
+                setCities(data);
+            } catch (e) {
+                console.error("couldn't fetch data", e);
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchCities();
+    }, []);
+
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Homepage/>}/>
+                    <Route index element={<Homepage/>}/>
                     <Route path="/product" element={<Product/>}/>
                     <Route path="/pricing" element={<Pricing/>}/>
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/app" element={<AppLayout/>}>
-                        <Route index element={<p>List Of Cities</p>} />
-                        <Route path='cities' element={<p>List Of Cities</p>} />
-                        <Route path='countries' element={<p>List Of Countries</p>} />
-                        <Route path='form' element={<p>Form</p>} />
+                        <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                        <Route path='cities' element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                        <Route path='countries' element={<p>List Of Countries</p>}/>
+                        <Route path='form' element={<p>Form</p>}/>
                     </Route>
                     <Route path="*" element={<PageNotFound/>}/>
                 </Routes>
---
+++ src/components/CityList/CityList.jsx
@@ -0,0 +1,18 @@
+import styles from './CityList.module.css';
+import Spinner from "../Spinner/Spinner.jsx";
+import CityItem from "../CityItem/CityItem.jsx";
+import Message from "../Message/Message.jsx";
+
+function CityList({cities, isLoading}) {
+    if (isLoading) return <Spinner/>;
+    if (!cities.length) return <Message/>;
+    return (
+        <ul className={styles.cityList}>
+            {cities.map((city) => {
+                return <CityItem city={city} key={city.id}/>
+            })}
+        </ul>
+    );
+}
+
+export default CityList;
